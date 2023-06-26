820703feae8395ea8c27ef6c38e84ad350a7da56
Zingworks-Sachin <sachin.pandit@zingworks.in>
2023-06-26 19:01:35 +0530

    Bug fixes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,59 +14,6 @@ void main() {
   runApp(const MyHomePage(title: 'Twilio Plugin',));
 }
 
-// class MyApp extends StatefulWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   State<MyApp> createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   // String _platformVersion = 'Unknown';
-//   // final _twilioChatConversationPlugin = TwilioChatConversation();
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     // initPlatformState();
-//   }
-//
-//   // Platform messages are asynchronous, so we initialize in an async method.
-//   // Future<void> initPlatformState() async {
-//   //   String platformVersion;
-//   //   // Platform messages may fail, so we use a try/catch PlatformException.
-//   //   // We also handle the message potentially returning null.
-//   //   try {
-//   //     platformVersion =
-//   //         await _twilioChatConversationPlugin.getPlatformVersion() ?? 'Unknown platform version';
-//   //   } on PlatformException {
-//   //     platformVersion = 'Failed to get platform version.';
-//   //   }
-//   //
-//   //   // If the widget was removed from the tree while the asynchronous platform
-//   //   // message was in flight, we want to discard the reply rather than calling
-//   //   // setState to update our non-existent appearance.
-//   //   if (!mounted) return;
-//   //
-//   //   setState(() {
-//   //     _platformVersion = platformVersion;
-//   //   });
-//   // }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Plugin example app'),
-//         ),
-//         body: Center(
-//           child: Text('Running on: $_platformVersion\n'),
-//         ),
-//       ),
-//     );
-//   }
-// }
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
