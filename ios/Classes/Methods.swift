d4e1039495df025af8c28f68725074b6a157c663
Zingworks-Sachin <sachin.pandit@zingworks.in>
2023-07-05 19:10:07 +0530

    Twilio support for iOS platform

---
+++ ios/Classes/Methods.swift
@@ -0,0 +1,22 @@
+//
+//  Methods.swift
+//  twilio_chat_conversation
+//
+//  Created by Zingworks-MBP-1 on 28/06/23.
+//
+
+import Foundation
+
+class Methods {
+    
+    /// The method channel name used to interact with the native platform.
+    static let generateToken: String = "generateToken"
+    static let createConversation: String = "createConversation"
+    static let getConversations: String = "getConversations"
+    static let getMessages: String = "getMessages"
+    static let joinConversation: String = "joinConversation"
+    static let sendMessage: String = "sendMessage"
+    static let addParticipant: String = "addParticipant"
+    static let receiveMessages: String = "receiveMessages"
+    static let getParticipants: String = "getParticipants"
+}
---
+++ ios/Classes/Strings.swift
@@ -0,0 +1,28 @@
+//
+//  Strings.swift
+//  twilio_chat_conversation
+//
+//  Created by Zingworks-MBP-1 on 28/06/23.
+//
+
+import Foundation
+
+class Strings {
+    
+    /// Used when conversation is created successfully
+    static let createConversationSuccess: String = "Conversation created successfully."
+
+    /// Used when there is an error while creating a conversation
+    static let createConversationFailure: String = "Error while creating conversation."
+    
+    static let conversationExists: String = "Conversation with provided name already exists"
+    
+    /// Used when participant is added to a conversation successfully
+    static let addParticipantSuccess: String = "Participant added successfully."
+    
+    /// Used when authentication is successful
+    static let authenticationSuccessful: String = "Authentication Successful"
+    
+    /// Used when authentication is unsuccessful
+    static let authenticationFailed: String = "Authentication Failed"
+}
